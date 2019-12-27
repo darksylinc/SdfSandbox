a936860a9b2848c462b628bc0c0a14bce298ece2
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2019-12-27 18:04:49 -0300

    Added custom file for toying around, and monitoring this file

--- include/SdfSandboxGameState.h
+++ include/SdfSandboxGameState.h
@@ -3,22 +3,34 @@
 #define _Demo_SdfSandboxGameState_H_
 
 #include "OgrePrerequisites.h"
+
 #include "TutorialGameState.h"
+
+#include <stdint.h>
+
+struct assetsys_t;
+struct filewatch_t;
 
 namespace Demo
 {
 	class SdfSandboxGameState : public TutorialGameState
 	{
-		Ogre::HlmsComputeJob *mSdfJob;
+		Ogre::HlmsComputeJob *m_sdfJob;
+
+		assetsys_t *m_assetSys;
+		filewatch_t *m_filewatch;
 
 		virtual void generateDebugText( float timeSinceLast, Ogre::String &outText );
 
 	public:
 		SdfSandboxGameState( const Ogre::String &helpDescription );
 
-		virtual void createScene01( void );
+		virtual void createScene01();
+		virtual void destroyScene();
 
 		virtual void update( float timeSinceLast );
+
+		void _watchCallback( uint32_t change, const char *virtualPath );
 
 		virtual void keyReleased( const SDL_KeyboardEvent &arg );
 	};
--- src/SdfSandboxGameState.cpp
+++ src/SdfSandboxGameState.cpp
@@ -20,43 +20,91 @@
 #include "OgreHlmsPbs.h"
 #include "OgreRoot.h"
 
+#include "OgreGpuProgramManager.h"
 #include "OgreHlmsCompute.h"
 #include "OgreHlmsComputeJob.h"
+
+#define ASSETSYS_IMPLEMENTATION
+#include "assetsys.h"
+#define CUTE_FILEWATCH_IMPLEMENTATION
+#include "cute_filewatch.h"
 
 using namespace Demo;
 
 namespace Demo
 {
+	static void watchCallback( filewatch_update_t change, const char *virtualPath, void *udata )
+	{
+		SdfSandboxGameState *gameState = reinterpret_cast<SdfSandboxGameState *>( udata );
+		gameState->_watchCallback( change, virtualPath );
+	}
+
 	SdfSandboxGameState::SdfSandboxGameState( const Ogre::String &helpDescription ) :
 		TutorialGameState( helpDescription ),
-		mSdfJob( 0 )
+		m_sdfJob( 0 ),
+		m_assetSys( 0 ),
+		m_filewatch( 0 )
 	{
+		m_assetSys = assetsys_create( 0 );
+		m_filewatch = filewatch_create( m_assetSys, 0 );
+
+		filewatch_mount( m_filewatch, "../Data/UserFiles", "/userfiles" );
+		filewatch_start_watching( m_filewatch, "/userfiles", watchCallback, this );
 	}
 	//-----------------------------------------------------------------------------------
-	void SdfSandboxGameState::createScene01( void )
+	void SdfSandboxGameState::createScene01()
 	{
 		mCameraController = new CameraController( mGraphicsSystem, false );
 
 		Ogre::HlmsCompute *hlmsCompute = mGraphicsSystem->getRoot()->getHlmsManager()->getComputeHlms();
-		mSdfJob = hlmsCompute->findComputeJob( "SdfRenderer" );
+		m_sdfJob = hlmsCompute->findComputeJob( "SdfRenderer" );
 
 		TutorialGameState::createScene01();
 	}
 	//-----------------------------------------------------------------------------------
+	void SdfSandboxGameState::destroyScene()
+	{
+		filewatch_stop_watching( m_filewatch, "/data" );
+
+		filewatch_free( m_filewatch );
+		assetsys_destroy( m_assetSys );
+
+		TutorialGameState::destroyScene();
+	}
+	//-----------------------------------------------------------------------------------
 	void SdfSandboxGameState::update( float timeSinceLast )
 	{
-		Ogre::ShaderParams &shaderParams = mSdfJob->getShaderParams( "default" );
+		Ogre::ShaderParams &shaderParams = m_sdfJob->getShaderParams( "default" );
 		shaderParams.setDirty();
 
 		Ogre::Quaternion camRot = mGraphicsSystem->getCamera()->getDerivedOrientation();
-//		camRot = camRot.Inverse();
+		//		camRot = camRot.Inverse();
 		Ogre::Matrix3 camRot3x3;
 		camRot.ToRotationMatrix( camRot3x3 );
 
 		shaderParams.mParams[0].setManualValue( mGraphicsSystem->getCamera()->getDerivedPosition() );
 		shaderParams.mParams[1].setManualValue( camRot3x3 );
 
+		filewatch_update( m_filewatch );
+		filewatch_notify( m_filewatch );
+
 		TutorialGameState::update( timeSinceLast );
+	}
+	//-----------------------------------------------------------------------------------
+	void SdfSandboxGameState::_watchCallback( uint32_t _change, const char *virtualPath )
+	{
+		const filewatch_update_t change = static_cast<filewatch_update_t>( _change );
+		if( change == FILEWATCH_FILE_MODIFIED &&
+			strcmp( virtualPath, "/userfiles/CustomSdf_piece_cs.any" ) == 0 )
+		{
+			// Hot reload Compute shaders.
+			Ogre::Root *root = mGraphicsSystem->getRoot();
+			Ogre::HlmsManager *hlmsManager = root->getHlmsManager();
+
+			Ogre::Hlms *hlms = hlmsManager->getComputeHlms();
+			Ogre::GpuProgramManager::getSingleton().clearMicrocodeCache();
+			hlms->reloadFrom( hlms->getDataFolder() );
+		}
 	}
 	//-----------------------------------------------------------------------------------
 	void SdfSandboxGameState::generateDebugText( float timeSinceLast, Ogre::String &outText )
